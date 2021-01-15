cc3e96960bf12eb55461770e7cbccf23006c5770
Stan <573387383@QQ.COM>
2021-01-15 17:30:53 +0800

    d

---
+++ sc.h
@@ -0,0 +1,16 @@
+//
+//  sc.h
+//  scPO
+//
+//  Created by AnX on 2021/1/15.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface sc : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sc.m
@@ -0,0 +1,20 @@
+//
+//  sc.m
+//  scPO
+//
+//  Created by AnX on 2021/1/15.
+//
+
+#import "sc.h"
+
+@implementation sc
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

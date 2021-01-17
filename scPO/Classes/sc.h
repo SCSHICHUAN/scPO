2d70aca3152e5fb04a9c73d0f1c70bee1201f124
Stan <573387383@qq.com>
2021-01-17 12:17:03 +0800

    。

---
+++ scPO/Classes/sc.h
@@ -0,0 +1,16 @@
+//
+//  sc.h
+//  cc
+//
+//  Created by Stan on 2021/1/17.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface sc : UIView
+-(instancetype)init;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ scPO/Classes/sc.m
@@ -0,0 +1,30 @@
+//
+//  sc.m
+//  cc
+//
+//  Created by Stan on 2021/1/17.
+//
+
+#import "sc.h"
+
+@implementation sc
+
+-(instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self  set];
+    }
+    return self;
+}
+-(void)set
+{
+    self.backgroundColor = UIColor.redColor;
+    self.bounds = [UIScreen mainScreen].bounds;
+    UILabel *lab = [[UILabel alloc] initWithFrame:self.bounds];
+    [self addSubview:lab];
+    lab.font = [UIFont boldSystemFontOfSize:60];
+    lab.text = @"欢迎使用Stan Pod库";
+    lab.textAlignment = NSTextAlignmentCenter;
+}
+@end
